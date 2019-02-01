8ca8ae6471708d36aa8e601cd5253bcae2a9a1b3
jarnoveldhuis <jarno@bloglovin.com>
2019-02-01 12:46:28 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ function moreInfo(){
 
 function dice(){
   document.querySelector("p").innerHTML =
-  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br>A simple game of chance.";
+  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br><h1>DICE</h1><br>A simple game of chance.";
 
   document.querySelector(".btn").outerHTML = "<a href='Projects/Dicee Challenge/dicee.html' class='btn btn-lg btn-secondary info'>Try it</a>";
 }
 
 function drums(){
   document.querySelector("p").innerHTML =
-  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br>A drum kit sound board. Pretty straight forward.";
+  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br><h1>DRUM KIT</h1><br>A drum kit sound board. Pretty straight forward.";
 
   document.querySelector(".btn").outerHTML = "<a href='Projects/Drum Kit/index.html' class='btn btn-lg btn-secondary info'>Try it</a>";
 }
 
 function wholesomeFamilyGame(){
   document.querySelector("p").innerHTML =
-  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br>A ridiculous game I made for my friends.";
+  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br><h1>WHOLESEOME FAMILY GAME</h1><br>A ridiculous game I made for my friends.";
 
   document.querySelector(".btn").outerHTML = "<a href='Projects/Simon Game/index.html' class='btn btn-lg btn-secondary info'>Try it</a>";
 }
 
 function toDoList(){
   document.querySelector("p").innerHTML =
-  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br>A todo list app. I read that there aren't enough of these on the internet.";
+  "<img src='images/Logomark - Black.svg' width='50%' height='50%' class='logo'><br><h1>ToDo List</h1><br>An app that keeps track of things to do. I read that the internet needs more of these.";
 
   document.querySelector(".btn").outerHTML = "<form class='form' autocomplete='off' onsubmit='createList()'><div class='form-group row'><label for='listName' class='col-sm-4 col-form-label'>Name your list:</label><div class='col-sm-4'><input type='text' class='form-control' id='inputName' placeholder='List Name'></div><div class='col-sm-3'><button type='submit' id='submitButton' class='btn btn-primary'>Create List</button></div></form>";
 }
